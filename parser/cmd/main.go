28f9835f0551aebcd1148f1d7bb8edb7d8fa8238
RadovanTomik <tomasik@mail.muni.cz>
2022-11-29 15:44:13 +0100

    refactor: project structure

--- parser/cmd/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"github.com/data_integration_module/parser/pkg"
-)
-
-func main() {
-	pkg.ReadXMLFile("/Users/radot/Projects/data_integration_module/parser/test/data/mmci_1.xml")
-}
