1bf479bea2e7a70bb584627b2fe7a03192c0de3c
RadovanTomik <tomasik@mail.muni.cz>
2023-01-09 20:57:01 +0100

    refactoring: dependency injection

--- biobank/biobank_service.py
+++ biobank/biobank_service.py
@@ -8,21 +8,13 @@ log = getCustomLogger(__name__)
 
 
 class BiobankService:
-    dirPath = None
 
     def __init__(self, biobank_repository: BiobankRecordRepository):
         self._biobankRecordRepository = biobank_repository
-        self._fileParser = FileParser(dir_path=self.dirPath)
 
-    def saveFilesIntoDBInJson(self) -> Cursor:
-        self.__validateDirPath()
-        for record in self._fileParser.parseXMLFilesInDir():
+    def saveFilesContentIntoDBInJson(self, file_parser: FileParser) -> Cursor:
+        for record in file_parser.parseXMLFilesInDir():
             yield self._biobankRecordRepository.insert(record)
 
     def getAllRecords(self):
         return self._biobankRecordRepository.getAll()
-
-    def __validateDirPath(self):
-        if self.dirPath is None:
-            log.critical("Directory path for biobank records is not set!")
-            raise ValueError
--- tests/biobank/test_BiobankService.py
+++ tests/biobank/test_BiobankService.py
@@ -6,6 +6,7 @@ from testcontainers.postgres import PostgresContainer
 from biobank.biobank_record_repository import BiobankRecordRepository
 from biobank.biobank_service import BiobankService
 from database.database import Database
+from parser.file_parser import FileParser
 
 
 class TestBiobankService(unittest.TestCase):
@@ -15,12 +16,12 @@ class TestBiobankService(unittest.TestCase):
         cls._postgresContainer.start()
         url = cls._postgresContainer.get_connection_url().split("+")[1].replace("psycopg2", "postgresql")
         Database.connectionUrl = url
-        BiobankService.dirPath = os.path.dirname(__file__) + "/../dummy_files"
         biobankRepository = BiobankRecordRepository(Database())
         cls._biobankService = BiobankService(biobankRepository)
 
     def test_saveFilesIntoDBInJson(self):
-        for cursor in self._biobankService.saveFilesIntoDBInJson():
+        file_parser = FileParser(os.path.dirname(__file__) + "/../dummy_files")
+        for cursor in self._biobankService.saveFilesContentIntoDBInJson(file_parser):
             self.assertEquals(cursor.statusmessage, "INSERT 0 1")
         self.assertEquals(len(self._biobankService.getAllRecords()), 2)
 
