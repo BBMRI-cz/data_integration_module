50ad8df897c69a8ef4483c24c2bd6e468bbe5985
RadovanTomik <tomasik@mail.muni.cz>
2023-01-10 13:54:23 +0100

    feat: init in main

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
+import os
+
+from biobank.biobank_record_repository import BiobankRecordRepository
+from biobank.biobank_service import BiobankService
+from database.database import Database
+from parser.file_parser import FileParser
+
 if __name__ == '__main__':
-    p
+    db = Database("postgresql://localhost:5432/dim?user=dim&password=dim")
+    biobankRepository = BiobankRecordRepository(db)
+    biobankService = BiobankService(biobankRepository)
+    file_parser = FileParser(os.path.dirname(__file__) + "/tests/dummy_files")
+    biobankService.saveFilesContentIntoDBInJson(file_parser)
