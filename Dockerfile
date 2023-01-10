fec228f1f49c980c8574ddc357e01653d5d2eb9f
Radovan Tomasik <tomasik@mail.muni.cz>
2023-01-10 16:11:07 +0100

    feat: workflow

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,15 @@
-FROM
+FROM python:3.11-alpine
+RUN apk update && \
+apk add --no-cache --virtual build-deps gcc python3-dev musl-dev && \
+apk add postgresql-dev
+ENV APP_DIR="/opt/dim"
+ENV RECORDS_DIR="/opt/dim/tests/dummy_files"
+RUN mkdir -p $APP_DIR
+WORKDIR $APP_DIR
+RUN python -m venv $APP_DIR/venv
+ENV PATH="$APP_DIR:$PATH"
+COPY --chown=1001:1001 . .
+RUN pip install -r requirements.txt
+USER 1001
+ENV PATH="/usr/app/venv/bin:$PATH"
+CMD ["pytest"]
--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from database.database import Database
 from parser.file_parser import FileParser
 
 if __name__ == '__main__':
-    db = Database("postgresql://localhost:5432/dim?user=dim&password=dim")
+    db = Database(os.getenv("DB_URL"))
     biobankRepository = BiobankRecordRepository(db)
     biobankService = BiobankService(biobankRepository)
-    file_parser = FileParser(os.path.dirname(__file__) + "/tests/dummy_files")
+    file_parser = FileParser(os.getenv("RECORDS_DIR"))
     biobankService.saveFilesContentIntoDBInJson(file_parser)
